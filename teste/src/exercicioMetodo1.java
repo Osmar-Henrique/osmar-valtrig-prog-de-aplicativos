4b1495f0fd90cfdd00251e35889a31a8f27ce06c
Osmar-Henrique <henrique.valtrig@gmail.com>
2026-08-11 21:46:24 -0300

    atualiza codigo

---
+++ teste/src/exercicioMetodo1.java
@@ -0,0 +1,21 @@
+public class exercicioMetodo1 {
+    public static void main(String[] args) {
+        escolhaNumero();
+        seForVerao();
+        seForInverno();
+    }
+
+
+        public static void escolhaNumero(){
+            System.out.println("Digite um número: ");
+        }
+
+        public static void seForVerao(){
+            System.out.println("É verão. O tempo está quente!");
+        }
+
+        public static void seForInverno(){
+            System.out.println("É inverno. E está frio!");
+        }
+
+    }
