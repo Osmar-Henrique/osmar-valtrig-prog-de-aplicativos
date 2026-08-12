5595a602c9226ca0bee9feb1b15a7a46cfd624b3
Osmar-Henrique <henrique.valtrig@gmail.com>
2026-08-11 21:28:31 -0300

    atualiza aula

---
+++ teste/src/aulaMetodo1.java
@@ -0,0 +1,13 @@
+public class aulaMetodo1 {
+    public static void main(String[] args) {
+        introduzirProfessor();
+        introduzirAluno();
+    }
+        public static void introduzirProfessor(){
+            System.out.println("O professor se chama Henrique");
+        }
+
+        public static void introduzirAluno(){
+            System.out.println("O aluno se chama Osmar");
+        }
+    }
