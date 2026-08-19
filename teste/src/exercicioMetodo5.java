16d2e934bed4f232c919e80da94218d860677444
Osmar-Henrique <henrique.valtrig@gmail.com>
2026-08-18 21:38:37 -0300

    atualiza

---
+++ teste/src/exercicioMetodo5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class exercicioMetodo5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+
+
+
+        numeroBase(numeroBs);
+
+    }
+
+    public static void numeroBase (double base, double n1, double n2){
+        
+    }
+
+}
