afb3582d73b56336ec8720d0e7823886cedeea2d
Osmar-Henrique <henrique.valtrig@gmail.com>
2026-08-19 21:11:43 -0300

    atualiza

---
+++ teste/src/exercicioMetodo8.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class exercicioMetodo8 {
+
+    public static double mediaAritimetica (double n1, double n2, double n3)
+    {
+        return (n1 + n2 + n3)/3;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite a primeira nota: ");
+        double nota1 = input.nextDouble();
+
+        System.out.println("Digite a segunda nota: ");
+        double nota2 = input.nextDouble();
+
+        System.out.println("Digite a terceira nota: ");
+        double nota3 = input.nextDouble();
+
+        System.out.println(mediaAritimetica(nota1,nota2,nota3));
+
+    }
+}
