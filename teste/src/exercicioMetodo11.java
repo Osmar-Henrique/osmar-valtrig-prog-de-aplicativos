6c80be5770a4783982a04710191c507083d2983b
Osmar-Henrique <henrique.valtrig@gmail.com>
2026-08-25 21:31:40 -0300

    atualiza

---
+++ teste/src/exercicioMetodo11.java
@@ -0,0 +1,30 @@
+public class exercicioMetodo11 {
+
+    public static int maiorVetor (int vetor[])
+    {
+        int vetorMaior = Integer.MIN_VALUE;
+        for (int i = 0; i < vetor.length; i++)
+        {
+            if (vetor[i] > vetorMaior)
+            {
+
+            }
+        }
+    }
+    public static int menorVetor (int vetor[])
+    {
+         = {1,2,3,4,5};
+
+        for (int j = 0; j < vetor.length; j++)
+        {
+            System.out.println(vetor[j]);
+            return vetor[j];
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
