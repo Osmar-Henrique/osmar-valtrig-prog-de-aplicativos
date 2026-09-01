cd9732fcb4e54399c1ba85c71e76e96b3dd57a2d
Osmar-Henrique <henrique.valtrig@gmail.com>
2026-09-01 20:06:46 -0300

    teste 02

--- teste/src/exercicioMetodo11.java
+++ teste/src/exercicioMetodo11.java
@@ -10,16 +10,18 @@ public class exercicioMetodo11 {
 
             }
         }
+        return 0;
     }
     public static int menorVetor (int vetor[])
     {
-         = {1,2,3,4,5};
+
 
         for (int j = 0; j < vetor.length; j++)
         {
             System.out.println(vetor[j]);
             return vetor[j];
         }
+        return 0;
 
     }
 
---
+++ teste/src/teste_Git.java
@@ -0,0 +1,9 @@
+public class teste_Git {
+    public static void main(String[] args) {
+
+        String teste = "Teste github";
+
+
+        System.out.println(teste);
+    }
+}
