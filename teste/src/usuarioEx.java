cfcaf25e0ba723f512640e2168efcdef5cc5e672
Osmar-Henrique <henrique.valtrig@gmail.com>
2026-09-01 20:18:35 -0300

    ex1

---
+++ teste/src/usuarioEx.java
@@ -0,0 +1,12 @@
+public class usuarioEx {
+
+    // Atributos (características) de um Usuario.
+    // Cada objeto Usuario terá seus próprios valores para esses atributos.
+    String nome;
+
+    char sexo;
+
+    String dataNascimento;
+
+    String estadoCivil;
+}
